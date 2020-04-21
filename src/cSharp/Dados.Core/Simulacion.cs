58d6223ff083655cd13dea62c571d32f347c2222
luchoxx87 <neoluis87@gmail.com>
2020-04-20 22:23:42 -0300

    Agregado test para la Simulación

--- src/cSharp/Dados.Core/Simulacion.cs
+++ src/cSharp/Dados.Core/Simulacion.cs
@@ -6,6 +6,17 @@ namespace Dados.Core
 {
     public class Simulacion
     {
+        public DateTime FechaHora { get; private set; }
+
+        /// <summary>
+        /// Propiedad que devuelve el intervalo de tiempo desde que se reseteo el cronómetro
+        /// </summary>
+        public TimeSpan Duracion => DateTime.Now - FechaHora;
+        public Simulacion()
+        {
+            ResetearCronometro();
+        }
+        public void ResetearCronometro() => FechaHora = DateTime.Now;
         public int SimularSinHilos(Dado dado, byte numero, int cantidadSimulaciones)
             => dado.TirarNVeces(numero, cantidadSimulaciones);
         public int SimularConHilos(Dado dado, byte numero, int cantidadSimulaciones, int cantidadHilos)
@@ -13,7 +24,7 @@ namespace Dados.Core
             Task<int>[] tareas = new Task<int>[cantidadHilos];
             int simulacionesPorHilo = cantidadSimulaciones / cantidadHilos;
             
-            //Instanciamos cada hilo, clonando el dado                     
+            //Instanciamos cada hilo, clonando un dado por cada hilo                     
             for (int i = 0; i < cantidadHilos; i++)
             {
                 Dado clon = (Dado)dado.Clone();                
---
+++ src/cSharp/Dados.Test.PruebaSimple/SimulacionDadoUnaCara.cs
@@ -0,0 +1,40 @@
+﻿using Dados.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dados.Test.PruebaSimple
+{
+    [TestClass]
+    public class SimulacionDadoUnaCara
+    {
+        public Simulacion Simulacion { get; set; }
+        public Dado Dado { get; set; }
+
+        [TestInitialize]
+        public void Iniciar()
+        {
+            Simulacion = new Simulacion();
+            Dado = new Dado(1);
+        }
+
+        [TestMethod]
+        public void GanarNVeces()
+        {
+            // Dependiento de la computadora que tengan, este test puede llevar cierto tiempo.
+            int cantidadSimulaciones = 20000000;
+            int cantidadHilos = 4;
+
+            Simulacion.ResetearCronometro();
+            var cantidadSinHilos = Simulacion.SimularSinHilos(Dado, 1, cantidadSimulaciones);
+            var duracionSinHilos = Simulacion.Duracion;
+
+            Simulacion.ResetearCronometro();
+            var cantidadConHilos = Simulacion.SimularConHilos(Dado, 1, cantidadSimulaciones, cantidadHilos);
+            var duracionConHilos = Simulacion.Duracion;
+
+            Assert.AreEqual(cantidadSimulaciones, cantidadSinHilos);
+            Assert.AreEqual(cantidadSimulaciones, cantidadConHilos);
+
+            Assert.IsTrue(duracionConHilos < duracionSinHilos);
+        }
+    }
+}
